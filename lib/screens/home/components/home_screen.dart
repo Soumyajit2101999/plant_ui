2960dbe2e218e7dda14ab742b4e5b041176d3429
Soumyajit Das <soumyajitdas2101999@gmail.com>
2022-12-20 01:18:21 +0530

    lastCommit

---
+++ lib/screens/home/components/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset("assets/icons/menu.svg"),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
