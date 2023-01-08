3551370ad5624bafea40178256485674d74702ba
Soumyajit Das <soumyajitdas2101999@gmail.com>
2023-01-09 00:42:40 +0530

    new commit tonight

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -5,6 +5,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plat_app_ui_practice/screens/home/components/header_with_searchbox.dart';
+import 'package:plat_app_ui_practice/screens/home/components/title_with_more_btn.dart';
 import 'package:plat_app_ui_practice/utils/constants.dart';
 
 class Body extends StatelessWidget {
@@ -17,50 +18,10 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           HeaderWithSearchBox(size: size),
-          Row(
-            children: [
-              TitleWithCustomUnderline(
-                text: "Recomended",
-              ),
-              // FlatButton
-            ],
+          TitleWithMoreButton(
+            title: "More",
+            press: () {},
           )
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithCustomUnderline extends StatelessWidget {
-  const TitleWithCustomUnderline({
-    Key? key,
-    required this.text,
-  }) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24,
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-            child: Text(
-              text,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                margin: const EdgeInsets.only(right: kDefaultPadding / 4),
-                height: 7,
-                color: kPrimaryColor.withOpacity(0.2),
-              ))
         ],
       ),
     );
---
+++ lib/screens/home/components/title_with_more_btn.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:plat_app_ui_practice/screens/home/components/title_with_custom_underline.dart';
+
+import '../../../utils/constants.dart';
+
+class TitleWithMoreButton extends StatelessWidget {
+  const TitleWithMoreButton({
+    Key? key,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
+
+  final String title;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TitleWithCustomUnderline(
+            text: "Recomended",
+          ),
+          TextButton(
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  // side: BorderSide(color: Colors.red)
+                )),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(kPrimaryColor),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+              ),
+              onPressed: () {},
+              child: Text(title))
+        ],
+      ),
+    );
+  }
+}
